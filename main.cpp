da010d284c637abe69d7f890c03de9096107414f
sharmavins23 <sharmavins23@gmail.com>
2022-12-28 01:12:59 -0500

    added ybuffer files and polygons

--- main.cpp
+++ main.cpp
@@ -12,45 +12,69 @@ Model *model = NULL;
 const int width = 800;
 const int height = 800;
 
-// Create a line between (x0, y0) -> (x1, y1) on the image with the color
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+// ===== Geometry functions ====================================================
+
+// Create a line between (p0.x, p0.y) -> (p1.x, p1.y) on the image with color
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
     // If this is a "steep" line, we treat it separately (to fill in dots
     // easily)
     bool steep = false;
-    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
+    if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) {
+        std::swap(p0.x, p0.y);
+        std::swap(p1.x, p1.y);
         steep = true;
     }
 
-    // Let's also fix missing lines by swapping so x0 < x1 always
-    if (x0 > x1) {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
-    }
+    // Let's also fix missing lines by swapping so p0.x < p1.x always
+    if (p0.x > p1.x) std::swap(p0, p1);
 
-    // Compute slopes
-    int dx = x1 - x0;
-    int dy = y1 - y0;
-    int derror2 = std::abs(dy) * 2; // Optimizing out floating points
-    int error2 = 0;
+    // COmpute and place the values
+    for (int x = p0.x; x <= p1.x; x++) {
+        // This is a basic computation of y=mx+b
+        float t = (x - p0.x) / (float)(p1.x - p0.x);
+        int y = p0.y * (1. - t) + (p1.y * t);
 
-    int y = y0;
-    for (int x = x0; x <= x1; x++) {
-        if (steep) {
+        // Remember to flip the data for steep values
+        if (steep)
             image.set(y, x, color);
-        } else {
+        else
             image.set(x, y, color);
-        }
+    }
+}
 
-        error2 += derror2;
-        // If the error is non-negligible, factor it in
-        if (error2 > dx) {
-            y += (y1 > y0 ? 1 : -1);
-            error2 -= dx * 2;
+// Given three vertices t0, t1, t2, place a triangle on the image
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+    // Shortcut - No degenerate triangles
+    if (t0.y == t1.y && t0.y == t2.y) return;
+
+    // Swap around to get shorter values earlier
+    if (t0.y > t1.y) std::swap(t0, t1);
+    if (t0.y > t2.y) std::swap(t0, t2);
+    if (t1.y > t2.y) std::swap(t1, t2);
+
+    // Compute the overall height of the triangle
+    int totalHeight = t2.y - t0.y;
+
+    for (int i = 0; i < totalHeight; i++) {
+        bool secondHalf = i > (t1.y - t0.y) || t1.y == t0.y;
+        int segmentHeight = secondHalf ? (t2.y - t1.y) : (t1.y - t0.y);
+        float alpha = (float)i / totalHeight;
+        float beta =
+            (float)(i - (secondHalf ? (t1.y - t0.y) : 0)) / segmentHeight;
+
+        // Compute the A and B sides of the polygon
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = secondHalf ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+
+        if (A.x > B.x) std::swap(A, B);
+
+        for (int j = A.x; j <= B.x; j++) {
+            image.set(j, t0.y + i, color); // Note: t0.y + i != A.y
         }
     }
 }
+
+// ===== Driver code ===========================================================
 
 int main(int argc, char **argv) {
     if (2 == argc) {
@@ -61,21 +85,32 @@ int main(int argc, char **argv) {
 
     TGAImage image(width, height, TGAImage::RGB);
 
+    // Position the light within the scene
+    Vec3f light_dir(0, 0, -1);
     for (int i = 0; i < model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
+        Vec3f world_coords[3];
         for (int j = 0; j < 3; j++) {
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j + 1) % 3]);
-            int x0 = (v0.x + 1.) * width / 2.;
-            int y0 = (v0.y + 1.) * height / 2.;
-            int x1 = (v1.x + 1.) * width / 2.;
-            int y1 = (v1.y + 1.) * height / 2.;
-            line(x0, y0, x1, y1, image, white);
+            Vec3f v = model->vert(face[j]);
+            screen_coords[j] =
+                Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
+            world_coords[j] = v;
+        }
+        Vec3f n = (world_coords[2] - world_coords[0]) ^
+                  (world_coords[1] - world_coords[0]);
+        n.normalize();
+        float intensity = n * light_dir;
+        if (intensity > 0) {
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2],
+                     image,
+                     TGAColor(intensity * 255, intensity * 255, intensity * 255,
+                              255));
         }
     }
 
     image.flip_vertically(); // Origin at left bottom corner of the image
-    image.write_tga_file("img/wireframe.tga");
+    image.write_tga_file("img/polygons.tga");
     delete model;
     return 0;
 }
