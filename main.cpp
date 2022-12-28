8f6a55d52be5f941afdbfe7e03a4d68ebc28b585
sharmavins23 <sharmavins23@gmail.com>
2022-12-28 00:50:43 -0500

    created wireframe

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
+#include "geometry.h"
+#include "model.h"
 #include "tgaimage.h"
+#include <cmath>
+#include <iostream>
+#include <ostream>
+#include <vector>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+Model *model = NULL;
+const int width = 800;
+const int height = 800;
 
 // Create a line between (x0, y0) -> (x1, y1) on the image with the color
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
@@ -44,13 +53,29 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 }
 
 int main(int argc, char **argv) {
-    TGAImage image(100, 100, TGAImage::RGB);
+    if (2 == argc) {
+        model = new Model(argv[1]);
+    } else {
+        model = new Model("obj/african_head.obj");
+    }
 
-    line(17, 20, 80, 40, image, white);
-    line(20, 13, 40, 80, image, red);
-    line(80, 40, 13, 20, image, red);
+    TGAImage image(width, height, TGAImage::RGB);
+
+    for (int i = 0; i < model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        for (int j = 0; j < 3; j++) {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j + 1) % 3]);
+            int x0 = (v0.x + 1.) * width / 2.;
+            int y0 = (v0.y + 1.) * height / 2.;
+            int x1 = (v1.x + 1.) * width / 2.;
+            int y1 = (v1.y + 1.) * height / 2.;
+            line(x0, y0, x1, y1, image, white);
+        }
+    }
 
     image.flip_vertically(); // Origin at left bottom corner of the image
-    image.write_tga_file("img/line.tga");
+    image.write_tga_file("img/wireframe.tga");
+    delete model;
     return 0;
 }
